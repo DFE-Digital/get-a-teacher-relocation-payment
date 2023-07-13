5a86be47d7ed04d655ec4732bf1eefdd863d0e5e
Pablo M <pablo@advera.co.uk>
2023-07-13 13:23:51 +0200

    Update preferences for start and end service dates

--- app/controllers/system_admin/settings_controller.rb
+++ app/controllers/system_admin/settings_controller.rb
@@ -7,5 +7,25 @@ module SystemAdmin
     def edit
       @app_settings = AppSettings.current
     end
+
+    def update
+      @app_settings = AppSettings.current
+      @app_settings.update!(app_settings_params)
+
+      redirect_to(edit_settings_path)
+    end
+
+  private
+
+    def app_settings_params
+      params.require(:app_settings).permit(
+        :"service_start_date(1i)",
+        :"service_start_date(2i)",
+        :"service_start_date(3i)",
+        :"service_end_date(1i)",
+        :"service_end_date(2i)",
+        :"service_end_date(3i)",
+      )
+    end
   end
 end
--- spec/features/admin_console/settings_spec.rb
+++ spec/features/admin_console/settings_spec.rb
@@ -11,10 +11,47 @@ describe "Settings" do
     then_i_should_see_the_settings_page
   end
 
+  it "updates the app_settings page" do
+    given_i_am_signed_as_an_admin
+    when_i_visit_the_settings_page
+    and_i_update_the_settings_page
+    then_i_should_see_the_settings_updated
+  end
+
 private
 
   def when_i_visit_the_settings_page
     visit edit_settings_path
+  end
+
+  def and_i_update_the_settings_page
+    within(".service-start-date") do
+      fill_in("Day", with: 1)
+      fill_in("Month", with: 1)
+      fill_in("Year", with: 2023)
+    end
+
+    within(".service-end-date") do
+      fill_in("Day", with: 31)
+      fill_in("Month", with: 1)
+      fill_in("Year", with: 2023)
+    end
+
+    click_on "Save"
+  end
+
+  def then_i_should_see_the_settings_updated
+    within(".service-start-date") do
+      expect(find_field("Day").value).to eq("1")
+      expect(find_field("Month").value).to eq("1")
+      expect(find_field("Year").value).to eq("2023")
+    end
+
+    within(".service-end-date") do
+      expect(find_field("Day").value).to eq("31")
+      expect(find_field("Month").value).to eq("1")
+      expect(find_field("Year").value).to eq("2023")
+    end
   end
 
   def then_i_should_see_the_settings_page
