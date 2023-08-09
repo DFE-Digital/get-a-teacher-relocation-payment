40ae21da55d15a056725d749c9fe497b24866af6
JR <139925286+fumimowdan@users.noreply.github.com>
2023-08-09 17:30:56 +0100

    Enable secure cookie (#179)
    * Enable secure cookie
    
    * exclude /healcheck from ssl redirect
    
    ---------
    
    Co-authored-by: Raul Gracia <raulgracialario@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+  config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("healthcheck") } } }
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
