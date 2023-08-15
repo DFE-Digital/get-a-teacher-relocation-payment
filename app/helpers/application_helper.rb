9946ba408382f01072dc4193f645e248e8f9bc68
fumimowdan <tecknuovo@zemis.co.uk>
2023-08-15 15:26:26 +0100

    Copy update closed page
    Trello Card
    [closed-page](https://trello.com/c/debvYG4e/160-s-new-copy-for-closed-page)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def link_to_irp(name='international relocation payment (IRP)')
+    govuk_link_to(
+      name,
+      'https://www.gov.uk/government/publications/international-relocation-payments/international-relocation-payments',
+      { target: '_blank' },
+    )
+  end
+
+  def mailto_teach_in_england
+    govuk_link_to("teach.inengland@education.gov.uk", "mailto:teach.inengland@education.gov.uk")
+  end
+
+  def mailto_irp_express_interest
+    govuk_link_to("irp.expressinterest@education.gov.uk", "mailto:IRP.ExpressInterest@education.gov.uk")
+  end
 end
