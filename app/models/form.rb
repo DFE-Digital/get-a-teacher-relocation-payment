d880f4f5f89b28651b85f2c407bb5b1638a3c1e3
fumimowdan <tecknuovo@zemis.co.uk>
2023-08-23 12:44:15 +0100

    Add Form model
    This new model will collect applicant's information prior the
    submission point.

---
+++ app/models/form.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: forms
+#
+#  id                            :bigint           not null, primary key
+#  address_line_1                :string
+#  address_line_2                :string
+#  application_route             :string
+#  city                          :string
+#  date_of_birth                 :date
+#  date_of_entry                 :date
+#  email_address                 :string
+#  family_name                   :string
+#  given_name                    :string
+#  middle_name                   :string
+#  nationality                   :string
+#  one_year                      :boolean
+#  passport_number               :string
+#  phone_number                  :string
+#  postcode                      :string
+#  school_address_line_1         :string
+#  school_address_line_2         :string
+#  school_city                   :string
+#  school_headteacher_name       :string
+#  school_name                   :string
+#  school_postcode               :string
+#  sex                           :string
+#  start_date                    :date
+#  state_funded_secondary_school :boolean
+#  subject                       :string
+#  visa_type                     :string
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
+#
+class Form < ApplicationRecord
+  def teacher_route?
+    application_route == "teacher"
+  end
+
+  def trainee_route?
+    application_route == "salaried_trainee"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -94,6 +94,37 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_093918) do
     t.index ["application_id"], name: "index_qa_statuses_on_application_id"
   end
 
+  create_table "forms", force: :cascade do |t|
+    t.string "given_name"
+    t.string "middle_name"
+    t.string "family_name"
+    t.string "email_address"
+    t.string "phone_number"
+    t.date "date_of_birth"
+    t.string "nationality"
+    t.string "sex"
+    t.string "passport_number"
+    t.string "subject"
+    t.string "visa_type"
+    t.date "date_of_entry"
+    t.date "start_date"
+    t.string "address_line_1"
+    t.string "address_line_2"
+    t.string "city"
+    t.string "postcode"
+    t.string "application_route"
+    t.boolean "state_funded_secondary_school"
+    t.boolean "one_year"
+    t.string "school_name"
+    t.string "school_headteacher_name"
+    t.string "school_address_line_1"
+    t.string "school_address_line_2"
+    t.string "school_city"
+    t.string "school_postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "schools", force: :cascade do |t|
     t.string "name"
     t.string "headteacher_name"
