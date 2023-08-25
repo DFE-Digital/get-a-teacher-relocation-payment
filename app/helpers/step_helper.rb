7376fde5c24876cb7f1c80a528873806712ff759
fumimowdan <tecknuovo@zemis.co.uk>
2023-08-25 06:46:59 +0100

    Add Invisible Captcha into refactored flow

--- app/helpers/step_helper.rb
+++ app/helpers/step_helper.rb
@@ -3,4 +3,10 @@ module StepHelper
     back_path = StepFlow.previous_step_path(step)
     link_to(t("steps.back"), back_path, class: "govuk-back-link") if back_path
   end
+
+  def form_method(step)
+    return :post if step.form.new_record?
+
+    :patch
+  end
 end
