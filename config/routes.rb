9b232c3c0d7088b59ad87f164f6850a318e71270
fumimowdan <tecknuovo@zemis.co.uk>
2023-09-01 09:47:18 +0100

    Remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -19,22 +19,6 @@ Rails.application.routes.draw do
   get "/privacy", to: "pages#privacy"
   get "/sitemap", to: "pages#sitemap"
 
-  # namespace :applicants do
-  #   resources :application_routes, only: %i[new create]
-  #   resources :school_details, only: %i[new create edit]
-  #   resources :contract_details, only: %i[new create edit]
-  #   resources :contract_start_dates, only: %i[new create edit]
-  #   resources :subjects, only: %i[new create edit]
-  #   resources :teaching_details, only: %i[new create edit]
-  #   resources :visas, only: %i[new create edit]
-  #   resources :entry_dates, only: %i[new create edit]
-  #   resources :personal_details, only: %i[new create edit]
-  #   resources :employment_details, only: %i[new create edit]
-  #   resources :salaried_course_details, only: %i[new create edit]
-  #   resources :application_summary, only: %i[new create]
-  #   resource :submission, only: %i[show]
-  # end
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     get "/users/sign_out", to: "devise/sessions#destroy", as: :destroy_user_session
