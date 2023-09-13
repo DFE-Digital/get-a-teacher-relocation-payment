09ac7d9845395fe608493b616e5306af2e13aedd
fumimowdan <tecknuovo@zemis.co.uk>
2023-09-13 15:38:11 +0100

    Renamed env var
    GOVUK_NOTIFY_MAIL_NOTIFY_TEMPLATE_ID to GOVUK_NOTIFY_GENERIC_EMAIL_TEMPLATE_ID

--- app/mailers/govuk_notify_mailer.rb
+++ app/mailers/govuk_notify_mailer.rb
@@ -2,7 +2,7 @@ class GovukNotifyMailer < Mail::Notify::Mailer
   def application_submission
     @urn = params[:urn]
     view_mail(
-      Rails.configuration.x.govuk_notify.mail_notify_template_id,
+      Rails.configuration.x.govuk_notify.generic_email_template_id,
       to: params[:email],
       subject: t("mailer.application_submission.subject"),
     )
