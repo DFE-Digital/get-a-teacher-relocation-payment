7234d15294d2d3f52c2b6a2473735fe2ccf6e414
Raul Gracia <raulgracialario@gmail.com>
2023-09-13 11:26:50 +0100

    fix error when new applications are created for auditing (#245)

--- app/controllers/system_admin/audits_controller.rb
+++ app/controllers/system_admin/audits_controller.rb
@@ -2,7 +2,7 @@ module SystemAdmin
   class AuditsController < AdminController
     include Pagy::Backend
     def index
-      @pagy, @audits = pagy(Audited::Audit.all.order(created_at: :desc), items: 10)
+      @pagy, @audits = pagy(Audited::Audit.all.where.not(user_id: nil).order(created_at: :desc), items: 10)
     end
   end
 end
