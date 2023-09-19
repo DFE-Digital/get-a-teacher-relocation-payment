0731bede471c2ee505e3f1d6977349c30fbab575
fumimowdan <tecknuovo@zemis.co.uk>
2023-09-19 10:21:25 +0100

    Removed unused methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,37 +5,6 @@ class ApplicationController < ActionController::Base
 
   before_action :check_service_open!
 
-  delegate :application_route, to: :current_application
-  helper_method :application_route
-
-  def check_application!
-    return if session["application_id"]
-
-    redirect_to(root_path)
-  end
-
-  def current_application
-    Application.in_progress.find(session["application_id"])
-  end
-  helper_method :current_application
-
-  def current_applicant
-    current_application.applicant
-  end
-  helper_method :current_applicant
-
-  def check_teacher!
-    return unless application_route != "teacher"
-
-    redirect_to(new_applicants_application_route_path)
-  end
-
-  def check_trainee!
-    return unless application_route != "salaried_trainee"
-
-    redirect_to(new_applicants_application_route_path)
-  end
-
   def check_service_open!
     return if request.path == destroy_user_session_path # skip this for log out page
     return if Gatekeeper.application_open?
