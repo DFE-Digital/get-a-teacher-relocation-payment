ffd6d1407b5b501e6f8f399167957d2df72f75a5
fumimowdan <tecknuovo@zemis.co.uk>
2023-09-22 17:12:46 +0100

    Report Applications
    https://trello.com/c/T0Auju6T/207-add-a-new-option-to-reports-section-to-download-all-applicants-on-the-submitted-state

--- app/controllers/system_admin/reports_controller.rb
+++ app/controllers/system_admin/reports_controller.rb
@@ -6,6 +6,7 @@ module SystemAdmin
       report = find_report(params[:id])
       create_audit(action: "Downloaded #{report.class.to_s.capitalize} report")
 
+      # TODO: Execute report is background job
       send_data(report.csv, filename: report.name)
     end
 
@@ -16,6 +17,7 @@ module SystemAdmin
         home_office: Reports::HomeOffice.new,
         standing_data: Reports::StandingData.new,
         payroll: Reports::Payroll.new,
+        applications: Reports::Applications.new,
       }.with_indifferent_access.fetch(report_id)
     end
   end
