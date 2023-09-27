873ff949b8378eb3ef6e98c7770d9691838bf54d
fumimowdan <tecknuovo@zemis.co.uk>
2023-09-27 14:52:43 +0100

    Add GenerateUrns service
    This service will create all the available urns per application route.
    They will be randomized, unique and the next available urn to be taken
    will retreived with the Urn.next method.
    
    Once all the urn are exhausted this method will raise an error and the
    format of the URN will need updating ie increase the size of the
    suffix set.  Urn::MAX_SUFFIX

---
+++ spec/services/generate_urns_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe GenerateUrns do
+  subject(:service) { described_class }
+
+  describe ".call" do
+    it { fail }
+  end
+
+  describe ".generate" do
+    it { fail }
+  end
+end
