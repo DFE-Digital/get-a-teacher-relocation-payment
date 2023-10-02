589865ba689f79c8cdf2c0f54b845bbd15b75850
fumimowdan <tecknuovo@zemis.co.uk>
2023-10-02 13:47:13 +0100

    Add Forms funnel and Ineligible forms funnel
    These widget have the ability to change the date_range of the
    formsfunnelquery but defaults to the last 24 hours.
    
    The date range filter feature is soft launched at the moment and
    is only usable via the url
    `/system-admin/dashboard?date_range[unit]=hours&date_range[range_start]=48&date_range[range_end]=24`

--- app/controllers/system_admin/dashboard_controller.rb
+++ app/controllers/system_admin/dashboard_controller.rb
@@ -1,7 +1,20 @@
 module SystemAdmin
   class DashboardController < AdminController
     def show
-      @kpis = Kpis.new
+      @kpis = Kpis.new(**kpi_params)
+    rescue ArgumentError => e
+      flash[:alert] = e.message
+      redirect_to(dashboard_path)
+    end
+
+  private
+
+    def kpi_params
+      params
+        .fetch(:date_range, {})
+        .permit(:unit, :range_start, :range_end)
+        .to_hash
+        .symbolize_keys
     end
   end
 end
