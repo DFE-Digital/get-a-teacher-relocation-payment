267036340797419593d50431ed7ae2fd8990697e
fumimowdan <tecknuovo@zemis.co.uk>
2023-10-09 11:49:55 +0100

    Revert PR #276
    In favour of a pseudo random urn generator that is not reliant of the
    database.
    This change will simplies the codebase

--- app/models/urn.rb
+++ app/models/urn.rb
@@ -1,41 +1,38 @@
-# == Schema Information
-#
-# Table name: urns
-#
-#  id         :bigint           not null, primary key
-#  code       :string
-#  prefix     :string
-#  suffix     :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-class Urn < ApplicationRecord
-  class NoUrnAvailableError < StandardError; end
+# frozen_string_literal: true
 
-  PREFIX = "IRP".freeze
-  MAX_SUFFIX = 99_999
-  PADDING_SIZE = MAX_SUFFIX.to_s.size
-  VALID_CODES = {
-    "teacher" => "TE",
-    "salaried_trainee" => "ST",
-  }.freeze
+# Urn represents a Uniform Resource Name (URN) generator.
+# It generates a URN with a fixed prefix and a random alphanumeric suffix.
+#
+#
+# Example:
+#
+#   Urn.generate('teacher')          # => "IRPTE12345"
+#   Urn.generate('teacher')          # => "IRPTE12345"
+#   Urn.generate('salaried_trainee') # => "IRPST12345"
+#
+class Urn
+  attr_reader :value
+  attr_writer :suffix
 
-  def self.next(route)
-    code = VALID_CODES.fetch(route)
-    Urn.transaction do
-      urn = find_by!(code:)
-      urn.destroy!
-      urn.to_s
+  def self.generate(applicant_type)
+    code = applicant_type_code(applicant_type)
+    PREFIX + code + Array.new(LENGTH) { CHARSET.sample }.join
+  end
+
+  CHARSET = %w[0 1 2 3 4 5 6 7 8 9].freeze
+  PREFIX = "IRP"
+  LENGTH = 5
+  private_constant :CHARSET, :PREFIX, :LENGTH
+
+  def self.applicant_type_code(applicant_type)
+    case applicant_type
+    when "teacher"
+      "TE"
+    when "salaried_trainee"
+      "ST"
+    else
+      raise(ArgumentError, "Invalid applicant type: #{applicant_type}")
     end
-  rescue KeyError => e
-    Sentry.capture_exception(e)
-    raise(ArgumentError, "Unknown route #{route}")
-  rescue ActiveRecord::RecordNotFound => e
-    Sentry.capture_exception(e)
-    raise(NoUrnAvailableError, "There no more unique URN available for #{route}")
   end
-
-  def to_s
-    [prefix, code, sprintf("%0#{PADDING_SIZE}d", suffix)].join
-  end
+  private_methods :applicant_type_code
 end
