cbecf29070cb79a365b3ea5bf61f33a610925961
fumimowdan <tecknuovo@zemis.co.uk>
2023-10-18 14:27:40 +0100

    Fix migrations

--- db/migrate/20231009110217_add_application_indexes.rb
+++ db/migrate/20231009110217_add_application_indexes.rb
@@ -1,6 +1,6 @@
 class AddApplicationIndexes < ActiveRecord::Migration[7.0]
   def change
-    drop_table :urns # rubocop:disable Rails/ReversibleMigration
+    execute("DROP TABLE IF EXISTS urns") # rubocop:disable Rails/ReversibleMigration
     add_index :applications, :urn, unique: true
     add_index :applications, :application_route
   end
