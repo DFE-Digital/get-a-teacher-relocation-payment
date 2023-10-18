e7aeeae560be328ee0d0e330347fb32d6fadd13a
fumimowdan <tecknuovo@zemis.co.uk>
2023-10-18 14:27:40 +0100

    Fix migrations

--- db/migrate/20230630083744_add_default_values_to_boolean_fields.rb
+++ db/migrate/20230630083744_add_default_values_to_boolean_fields.rb
@@ -1,11 +1,14 @@
 class AddDefaultValuesToBooleanFields < ActiveRecord::Migration[7.0]
   def up
-    ApplicationProgress.where.not(visa_investigation_required: true)
-                       .update(visa_investigation_required: false)
-    change_column :application_progresses, :visa_investigation_required, :boolean, default: false, null: false
+    execute('UPDATE application_progresses
+      SET visa_investigation_required = false
+      WHERE visa_investigation_required != true')
 
-    ApplicationProgress.where.not(school_investigation_required: true)
-                        .update(school_investigation_required: false)
+    change_column :application_progresses, :visa_investigation_required, :boolean, default: false, null: false
+    execute('UPDATE application_progresses
+      SET school_investigation_required = false
+      WHERE school_investigation_required != true')
+
     change_column :application_progresses, :school_investigation_required, :boolean, default: false, null: false
   end
 end
