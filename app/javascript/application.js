c5efa0843b2357c3ae65eeb8b44c2656ea16f5d1
fumimowdan <tecknuovo@zemis.co.uk>
2023-10-23 15:16:28 +0100

    Disabled Js on application form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import { initAll } from "govuk-frontend"
+// import { initAll } from "govuk-frontend"
 
-initAll();
+// initAll();
