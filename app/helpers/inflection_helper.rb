6cf17779300d655558983997f589e47051624d9a
eddieleeper <eddie.leeper@gmail.com>
2024-01-10 17:01:53 +0000

    Update InflectionHelper to fix lint error

--- app/helpers/inflection_helper.rb
+++ app/helpers/inflection_helper.rb
@@ -1,4 +1,4 @@
-# Create a new file - app/helpers/inflection_helper.rb
+# Initially created to help display the correct form of day or days in the dashboard.
 module InflectionHelper
   def pluralize_word(count, word)
     "#{count} #{word.pluralize(count)}"
