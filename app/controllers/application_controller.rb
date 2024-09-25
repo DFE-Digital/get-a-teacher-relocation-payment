e91c58b12f870c1c1e6fa9ff048d1bef45d4cf43
Phil Lee <asmega@users.noreply.github.com>
2024-09-25 11:31:14 +0100

    Hard redirect offsite (#441)
    * redirect traffic to guidance pages
    
    * redirect closed service offsite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,10 @@ class ApplicationController < ActionController::Base
     return if request.path == destroy_user_session_path # skip this for log out page
     return if Gatekeeper.application_open?
 
-    redirect_to(closed_path)
+    redirect_to(
+      "https://getintoteaching.education.gov.uk/non-uk-teachers/get-an-international-relocation-payment",
+      allow_other_host: true,
+    )
   end
 
   def current_form
